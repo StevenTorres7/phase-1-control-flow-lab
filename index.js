a60fc9e4eb4d0338351571293a0727f7958573c6
Steven <Steventorres7@yahoo.com>
2023-07-06 20:29:44 -0400

    commit tree 152f817f67b1feadd5d9621404ebf363d0a498d3

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return('This one is on me!');
+  }
+  else if (400 < ride < 2000) {
+    return('That will be twenty bucks.');
+  }
+  else {
+    return('I will gladly take your thirty bucks.');
+  } 
 }
 
 function ternaryCheckCity(){
